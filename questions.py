bbb3bdacc74769e9a692569d4d87ab7eaa3ccc1c
PBackma11 <112958776+PBackma11@users.noreply.github.com>
2022-10-13 18:03:37 -0700

    first commit

---
+++ questions.py
@@ -0,0 +1,41 @@
+# Question 1
+# Write a function to print "hello_USERNAME!" 
+# USERNAME is the input of the function. 
+ 
+def hello_name(user_name):
+
+
+
+
+
+# Question 2
+# Write a python function, first_odds that prints the odd numbers from 1-100
+# and returns nothing 
+
+def first_odds():
+
+
+
+# Question 3
+# Write a Python function, max_num_in_list to return the max number of a given 
+# list. 
+
+def max_num_in_a_list(a_list):
+
+
+
+# Question 4
+# Write function to return if the given year is a leap year. A leap year is 
+# divisible by 4, but not divisible by 100 unless it is also divisible by 400.
+# The return should be boolean Type(true/false). 
+
+def is_leap_year(a_year):
+
+
+
+# Question 5
+# Write function to check to see if all numbers in the list are consecutive numbers.
+# For ex. [2,3,4,5,6,7] are consecutive, but [1,2,4,5] are not.
+# The return should be boolean Type.
+
+def is_consecutive(a_list):
